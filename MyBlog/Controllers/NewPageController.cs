8a1acbe5cc84d42eae72de9e9b6d056d1746589c
LiangXu <1256565740@qq.com>
2016-08-30 16:49:30 +0800

    新增测试页面

---
+++ MyBlog/Controllers/NewPageController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyBlog.Controllers
+{
+    public class NewPageController : Controller
+    {
+        // GET: NewPage
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: NewPage/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: NewPage/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: NewPage/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: NewPage/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: NewPage/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: NewPage/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: NewPage/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
